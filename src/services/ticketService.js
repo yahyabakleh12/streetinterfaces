1778334c1beaeb5217a7f3639e60b66ca4e11ab6
Yahya Bakleh <123378314+yahyabakleh12@users.noreply.github.com>
2025-08-12 13:26:42 +0400

    Display exit image instead of video in tickets

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -4,6 +4,7 @@ export default {
   getAll(params = {}) { return API.get('/tickets', { params }) },
   get(id)             { return API.get(`/tickets/${id}`) },
   getImage(id)        { return API.get(`/tickets/${id}/image`, { responseType: 'blob' }) },
+  getExitImage(id)    { return API.get(`/tickets/${id}/exit-image`, { responseType: 'blob' }) },
   getVideo(id)        { return API.get(`/tickets/${id}/video`, { responseType: 'blob' }) },
   create(payload)     { return API.post('/tickets', payload) },
   update(id, payload) { return API.put(`/tickets/${id}`, payload) },
